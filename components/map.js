7440cce5f425f91d775100b9f222f4f52cadceb7
Jason Biliguun <jasonbiliguun@Amgalans-MacBook-Pro.local>
2025-10-05 20:22:02 +0800

    added map and weather predict components

---
+++ components/map.js
@@ -0,0 +1,42 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const markerIcon = new L.Icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+export default function WeatherMap({ coords, setCoords }) {
+  function MapClickHandler() {
+    useMapEvents({
+      click(e) {
+        const { lat, lng } = e.latlng;
+        setCoords({ lat, lon: lng });
+      },
+    });
+    return null;
+  }
+
+  if (!coords) return <p className="text-center p-4">Loading map...</p>;
+
+  return (
+    <div className="h-80 mb-6 border rounded overflow-hidden">
+      <MapContainer
+        center={[coords.lat, coords.lon]}
+        zoom={5}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://osm.org">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[coords.lat, coords.lon]} icon={markerIcon} />
+        <MapClickHandler />
+      </MapContainer>
+    </div>
+  );
+}
